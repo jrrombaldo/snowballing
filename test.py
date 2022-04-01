435b26131279ddc609dca3dade3befacea5b5b8f
Carlos R <jr.rombaldo@gmail.com>
2022-04-01 16:28:08 +0100

    refactoring

--- test.py
+++
@@ -1,6 +0,0 @@
-from snowballing.scholarsemantic import ScholarSemantic
-
-
-if __name__ == "__main__":
-    for paper_tuple in ScholarSemantic().get_extracted_papers_to_snowball('both'):
-        print (paper_tuple)
