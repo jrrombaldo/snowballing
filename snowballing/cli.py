65c78548bc1c765402a6148b57a9b404c4ff195d
Carlos R <jr.rombaldo@gmail.com>
2022-04-01 11:51:22 +0100

    argpaser support

--- snowballing/cli.py
+++ snowballing/cli.py
@@ -6,24 +6,42 @@ def parse_cli_args():
         description=' Snowballing and metadata searcing...',
         epilog='happy research! :)')
 
+
     subparsers = parser.add_subparsers(dest="module", required=True, help="operation module")
     
     #  Search module
-    ris_parser = subparsers.add_parser('search ', 
+    ris_parser = subparsers.add_parser('search', 
     help='perform semanticscholar searchs')
-    ris_parser.add_argument('ris_file', 
-        metavar='RIS_file', 
-        type=argparse.FileType('r'),
-        help='RIS file containing studies')
+
 
     # Snowballing method
     rayyan_parser = subparsers.add_parser('snowballing', 
         help='whatever it does ...')
-    rayyan_parser.add_argument('ris_file', 
-        metavar='RIS_file', 
-        type=argparse.FileType('r'),
-        help='RIS file containing studies'
+    rayyan_parser.add_argument('--approach', 
+        metavar='<snowballing approach>',
+        required=True,
+        choices=['backward', 'forward', 'both'],
+        help='type of snowballing ...'
     )
+
+    parser.add_argument('ris_file', 
+        metavar='<RIS file>', 
+        type=argparse.FileType('r'),
+        help='RIS format file containing bibliography')
+    
+    parser.add_argument("--threads",
+        help='Number of threads (one thread per paper). Default (75)',
+        metavar='<nuumber of threads>', 
+        action='store',
+        required=False,
+        type=int,
+        default=1)
+    
+    parser.add_argument('--tor', 
+        help='use TOR networks, which thread will create a connection and will have different internet IP',
+        action='store_true', 
+        required=False,
+        default=False)
 
     args = parser.parse_args()
     return args
